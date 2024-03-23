46038ab838d5cb2f0c6dc2d31c71592db4c5458c
alya <120921108+capinowo@users.noreply.github.com>
2024-03-23 21:30:50 +0700

    Delete Praktikum 4/org/Tugas 4/bk directory

--- Praktikum 4/org/Tugas 4/bk/bukunonFiksi/bukuAkademik.java
+++
@@ -1,8 +0,0 @@
-package bk.bukunonFiksi;
-
-public class bukuAkademik extends  bukunonFiksi {
-    public bukuAkademik (String judul, String penulis, String tahunTerbit, String subjek, int jumlahHalaman, double harga) {
-        super(judul, penulis, tahunTerbit, subjek, jumlahHalaman, harga);
-    }
-    
-}
