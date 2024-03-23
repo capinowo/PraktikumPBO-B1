e12d90d2db38ff702608397b9a7dab6abc4a24e3
alya <120921108+capinowo@users.noreply.github.com>
2024-03-23 21:04:35 +0700

    Add files via upload

---
+++ Praktikum 4/org/Tugas 4/bk/bukunonFiksi/bukuAkademik.java
@@ -0,0 +1,8 @@
+package bk.bukunonFiksi;
+
+public class bukuAkademik extends  bukunonFiksi {
+    public bukuAkademik (String judul, String penulis, String tahunTerbit, String subjek, int jumlahHalaman, double harga) {
+        super(judul, penulis, tahunTerbit, subjek, jumlahHalaman, harga);
+    }
+    
+}
