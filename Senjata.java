ea25f554efb8ecd810f2e66b83cc5eacd8299ecd
alya <120921108+capinowo@users.noreply.github.com>
2024-05-07 23:37:09 +0700

    Add files via upload

---
+++ Senjata.java
@@ -0,0 +1,42 @@
+/*
+Nama        : Alya Safina
+NIM         : 24060122140123
+*/
+
+package latihanMedium;
+
+public class Senjata {
+    private String bunyi;
+    private boolean menusuk;
+    private int peluru;
+
+    public Senjata (String bunyi) {
+        this.bunyi = bunyi;
+        this.menusuk = false;
+        this.peluru = 0;
+    }
+
+    public String getBunyi () {
+        return bunyi;
+    }
+
+    public int getPeluru () {
+        return peluru;
+    }
+
+    public boolean isMenusuk () {
+        return menusuk;
+    }
+
+    public void setBunyi (String bunyi) {
+        this.bunyi = bunyi;
+    }
+
+    public void setMenusuk (boolean menusuk) {
+        this.menusuk = true;
+    }
+
+    public void setPeluru (int peluru) {
+        this.peluru = peluru;
+    }
+}
