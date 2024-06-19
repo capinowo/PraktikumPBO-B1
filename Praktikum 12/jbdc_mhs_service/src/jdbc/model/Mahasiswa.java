c3b4417b0d866c6082b7389947e90925b2fc6f23
alya safina(wa) <120921108+capinowo@users.noreply.github.com>
2024-06-19 10:23:47 +0700

    Update Mahasiswa.java

--- Praktikum 12/jbdc_mhs_service/src/jdbc/model/Mahasiswa.java
+++ Praktikum 12/jbdc_mhs_service/src/jdbc/model/Mahasiswa.java
@@ -9,5 +9,37 @@ package jdbc.model;
  * @author LENOVO
  */
 public class Mahasiswa {
+    private int id;
+    private String nama;
     
+    public Mahasiswa(){
+        id = 0;
+        nama = null;
+    }
+    
+    public Mahasiswa(int id, String nama) {
+        this.id = id;
+        this.nama = nama;
+    }
+    
+    public int getId() {
+        return id;
+    }
+    
+    public void setId(int id){
+        this.id = id;
+    }
+    
+    public String getNama(){
+        return nama;
+    }
+    
+    public void setNama(String nama){
+        this.nama = nama;
+    }
+    
+    @Override
+    public String toString(){
+        return "Mahasiswa{" + "id=" + id + ", nama=" + nama + "}";
+    } 
 }
